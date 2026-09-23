d8316dacbecdaaaea510010c1c8853532b6b6f13
ajmatias89 <ajmatias89@gmail.com>
2026-09-23 18:44:20 -0500

    Agergando proyecto con valor centinela, correctamente

---
+++ LibroCalificacionesPromedioClase2/LibroCalificacionesPromedioClase2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace LibroCalificacionesPromedioClase2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Fig. 5.6: PruebaLibroCalificaciones.cs
+            // Crea el objeto LibroCalificaciones e invoca a su método DeterminarPromedio.
+            // crea el objeto miLibroCalificaciones de LibroCalificaciones y
+            // pasa el nombre del curso al constructor
+
+            LibroCalificaciones miLibroCalificaciones = new LibroCalificaciones("CS101 Introducción a la programación en C#");
+
+            miLibroCalificaciones.MostrarMensaje(); // muestra el mensaje de bienvenida
+            miLibroCalificaciones.DeterminarPromedioClase(); // encuentra el promedio de 10 calificaciones
+        }
+    }
+}
